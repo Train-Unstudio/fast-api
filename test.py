00cd447d73f97ea5d8ecd1586a6961911e90c5ad
Train-Unstudio <129270359+Train-Unstudio@users.noreply.github.com>
2023-05-05 19:58:14 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -1,9 +1,17 @@
 import requests
-
+import base64
 url = 'http://localhost:8000/design/sd'
+
+def img2base64():
+    with open("/content/serum_standard.png","rb") as img_file:
+      encoded_string=base64.b64encode(img_file.read()).decode("utf-8")
+    return encoded_string
+b64_string=img2base64()
+
 data = {
-    'base64_string': '...your base64 string here...',
+    'base64_string': b64_string,
     'prompt': '...your prompt here...'
 }
 response = requests.post(url, data=data)
 print(response.json())
+
