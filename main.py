59201041d85c1c8d6f6a8e23d1b5c0d6ca49a456
Train-Unstudio <129270359+Train-Unstudio@users.noreply.github.com>
2023-05-05 19:41:57 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
+# main.py
+from model import *
+import requests
+import numpy as np
+import time
+import logging 
 
 from fastapi import FastAPI, HTTPException, Form, UploadFile, File, Response
 # from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-from PIL import Image
-import io
-from base64 import b64encode
+
 import os
-from model import *
-from typing import Union
-from io import BytesIO
-import base64
-import time
 import shutil
+import datetime
+import requests
+import io
+import cv2
+import base64
+from io import BytesIO
+from PIL import Image, ImageChops, ImageFile
+from io import BytesIO
+#from cv2 import dnn_superres
+from diffusers import StableDiffusionInpaintPipeline, ControlNetModel, UniPCMultistepScheduler
+import torch
+from pipeline_stable_diffusion_controlnet_inpaint import *
+from torch import autocast
+import numpy as np
+import io 
+import base64
+import cv2 
+from PIL import Image
+import torch
+import warnings
 
-
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 #import imageio as iio
 
-#app = FastAPI(title="Project Arch")
+app = FastAPI(title="Project Arch")
 
 origins = [
     "http://localhost",
@@ -31,33 +50,22 @@ origins = [
 #)
 
 
-#@app.get("/")
+@app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-#@app.post("/design/sd", status_code=200)
-def predict(withoutbg_image_base64,prompt):
-#def predict(withoutbg_image_base64 : str = Form(...), prompt : str = Form(...), randID: str = Form(...), username: str = Form(...), model_name: str = Form(...)):
-    #withoutbg_image_base64 = b'=' * (-len(withoutbg_image_base64) % 4)
-    #withoutbg_image_encoded = withoutbg_image_base64.encode('utf-8')
-    withoutbg_image_bytes = BytesIO(base64.b64decode(withoutbg_image_base64))
-    #withoutbg_image = Image.open(withoutbg_image_bytes)
-    image_base64 = generate(withoutbg_image_bytes, prompt, model_name = "model_1")
-    #image_base64 = generate(withoutbg_image, prompt, model_name = model_name)
+@app.post("/design/sd", status_code=200)
 
-    return {'image_base64': image_base64}
+# this accepts the base64encoded string 
+def gen_output(base64_string:str=Form(...),prompt:str=Form(...)):
+   custom_model(base64_string,prompt)
 
-
-def imgToBase64String(filename):
-    img = Image.open(filename)
-    im_file = BytesIO()
-    img.save(im_file, format="PNG")
-    im_bytes = im_file.getvalue()  # im_bytes: image in binary format.
-    im_b64 = base64.b64encode(im_bytes).decode('utf-8')
-    return im_b64
-
-withoutbg_image_string = imgToBase64String("fastapi/serum_standard.png") 
-
-predict(withoutbg_image_string,"serum on mountain")
-
+#test.py 
+#def img2base64():
+#    with open("/content/serum_standard.png","rb") as img_file:
+#      encoded_string=base64.b64encode(img_file.read()).decode("utf-8")
+#    return encoded_string
+#b64_string=img2base64()
+#prompt="serum on hills"
+#gen_output(b64_string,prompt)
--- model.py
+++ model.py
@@ -4,7 +4,6 @@ import time
 import logging 
 import os
 import shutil
-import boto3
 import datetime
 import requests
 import io
@@ -16,7 +15,6 @@ from io import BytesIO
 #from cv2 import dnn_superres
 from diffusers import StableDiffusionInpaintPipeline, ControlNetModel, UniPCMultistepScheduler
 import torch
-from pipeline_stable_diffusion_controlnet_inpaint import *
 from torch import autocast
 import numpy as np
 import io 
@@ -34,17 +32,12 @@ def get_mask_image3(init_img_sd):
     init_img_sd_array[np.where(init_img_sd_array[:, :, 3] > 0 )] = [255,255,255,255]
     init_img_sd_array[np.where(init_img_sd_array[:, :, 3] <= 0 )] = [0,0,0,255]
     out_img = Image.fromarray(init_img_sd_array)
+    out_img.save('mask_image_std_serum.png') # rename the file corresponding to the file name
+
 #     blur = GaussianBlur(11,20)
 #     out_img = blur(out_img)
     return out_img
 
-def process_init(init_image):
-    init_img_sd_array = np.array(init_image)
-    init_img_sd_array[np.where(init_img_sd_array[:, :, 3] <= 128 )] = [128,128,128,255]
-    out_img = Image.fromarray(init_img_sd_array)
-#     blur = GaussianBlur(11,20)
-#     out_img = blur(out_img)
-    return out_img
 
 def get_canny(image): 
   # get canny image
@@ -61,64 +54,32 @@ def get_canny(image):
   canny_image.save('canny_image_std_serum.png') # rename the file corresponding to the file name
   return canny_image
 
-def sd_controlnet_inpainting(init_image,prompt):
-    print("at sd controlnet_inpainting function")
-    #init_image_encoded = init_image.encode('utf-8')
-
-    #init_image_bytes = BytesIO(base64.b64decode(init_image))
-    im_bytes = init_image.getvalue() 
-    im_b64 = base64.b64encode(im_bytes).decode('utf-8')
-    #im_bytes.save("init_image.png")
-    init_image = Image.open(im_b64)
-    
-    mask_image=get_mask_image3(init_image)
-    canny_image=get_canny(init_image)
-    # load control net and stable diffusion v1-5
-    controlnet = ControlNetModel.from_pretrained("lllyasviel/sd-controlnet-canny", torch_dtype=torch.float16)
-    pipe = StableDiffusionControlNetInpaintPipeline.from_pretrained(
+def custom_model(b64string,prompt):
+   img_bytes=base64.b64decode(b64string)
+   img_stream=io.BytesIO(img_bytes)
+   img=Image.open(img_stream)
+   init_img=img
+   canny_image=get_canny(init_img)
+   mask_image=get_mask_image3(init_img)
+   init_img.save("init_img.png")
+   controlnet = ControlNetModel.from_pretrained("lllyasviel/sd-controlnet-canny", torch_dtype=torch.float16)
+   pipe = StableDiffusionControlNetInpaintPipeline.from_pretrained(
      "runwayml/stable-diffusion-inpainting", controlnet=controlnet, torch_dtype=torch.float16
  )
-# speed up diffusion process with faster scheduler and memory optimization
-    pipe.scheduler = UniPCMultistepScheduler.from_config(pipe.scheduler.config)
-    pipe.enable_xformers_memory_efficient_attention()
-    pipe.to('cpu')
-    generator = torch.manual_seed(-1)
-    output= pipe(
+   pipe.scheduler = UniPCMultistepScheduler.from_config(pipe.scheduler.config)
+   pipe.enable_xformers_memory_efficient_attention()
+   pipe.to('cuda')
+   generator = torch.manual_seed(-1)
+   output= pipe(
         prompt,
         num_inference_steps=20,
         generator=generator,
-        image=init_image,
+        image=init_img,
         control_image=canny_image,
         controlnet_conditioning_scale = 1.0,
         mask_image=mask_image
     ).images[0]
-    return output
-
-def generate(withoutbg_img, prompt, model_name = "model_1"):
-
-    print("at generate function in model.py")
-    withoutbg_img_new = withoutbg_img    
-
-
-    withoutbg_img_sd = withoutbg_img_new
-
-    #mask_img_sd_path = randID + "/" + "mask_img_sd_" + randID + ".png"
-    #canny_image_sd_path = randID + "/" + "canny_sd_" + randID + ".png"
-    #withoutbg_img_sd_path = randID + "/" + "withoutbg_img_sd_" + randID + ".png"
-
-    #canny_image.save(canny_image_sd_path)
-    #mask_img_sd.save(mask_img_sd_path)
-    #withoutbg_img_sd.save(withoutbg_img_sd_path)
-    if model_name == "model_1":
-        sd_output = sd_controlnet_inpainting(withoutbg_img_sd,prompt)
-        sd_output_2 = sd_output
-        sd_output_2.save("output_" +".jpeg")
-        #with open("output_" + ".jpeg", "rb") as img_file:
-
-        image_base64 = base64.b64encode(sd_output_2.read()).decode('utf-8')
-        img_bytes = base64.b64decode(image_base64.split(',')[1])
-        img_io = BytesIO(img_bytes)
-        img = Image.open(img_io)
-        img.save('output.png', 'PNG')
-
-    return image_base64
+   output.save("output.png")
+   with open ("output.png","rb") as image_file:
+    encoded_string=base64.b64encode(image_file.read()).decode("utf-8")
+   return encoded_string
